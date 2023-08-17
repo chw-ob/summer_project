538958c94abda1f8c63024e424bae4d60472ffca
chw-ob <2252466462@qq.com>
2023-08-17 20:57:18 +0800

    加入极大极小搜索方法

--- chw.py
+++ chw.py
@@ -22,7 +22,7 @@ class game():
         pass
     def judge_self(self):#judge一下游戏是否结束
         return self.judge(self.data)
-        pass#
+        pass
     def judge(self,data):
         #0:继续 1:player1胜利 2:player2胜利 3:平局
         is_game_over=False
@@ -36,7 +36,7 @@ class game():
         if data[0][0]==data[1][1]==data[2][2]!=0:
             is_game_over, winner = True, data[0][0]
         if data[2][0]==data[1][1]==data[0][2]!=0:
-            is_game_over, winner = True, data[0][0]
+            is_game_over, winner = True, data[0][2]
         if np.min(self.data)>0:
             return 3
         if is_game_over==True:
@@ -61,14 +61,14 @@ class search():
         pass
     def __call__(self, *args, **kwargs):
         return self.forward(*args,**kwargs)
-    def minimax(self,data,depth,game,is_min):
+    def minimax(self,depth,game,is_min):
         if game.judge_self() in [1,2,3] or depth==0:
             return self.evaluate(game)
         if is_min==True:
             max_eval=float('-inf')
             for move in self.get_possible_moves(game):
                 game.action(move)
-                eval=self.minimax(data,depth-1,game,False)
+                eval=self.minimax(depth-1,game,False)
                 max_eval=max(max_eval,eval)
                 game.daction(move)
             return max_eval
@@ -76,14 +76,14 @@ class search():
             min_eval=float('inf')
             for move in self.get_possible_moves(game):
                 game.action(move)
-                eval=self.minimax(data,depth-1,game,True)
+                eval=self.minimax(depth-1,game,True)
                 min_eval=min(min_eval,eval)
                 game.daction(move)
             return min_eval
         pass
     def evaluate(self,game):
         score=game.judge_self()
-        score_dir={1:{0:0,1:-1,2:1},2:{0:0,1:1,2:-1}}
+        score_dir={1:{3:0,1:-1,2:1,0:0},2:{3:0,1:1,2:-1,0:0}}
         return score_dir[self.player][score]
         pass
     def get_possible_moves(self,game):
@@ -91,26 +91,33 @@ class search():
         data=game.get_ob()
         for i in range(len(data)):
             for j in range(len(data[i])):
-                moves.append([i,j])
+                if data[i,j]==0:
+                    moves.append([i,j])
         return moves
         pass
-    def get_best_move(self,data,depth,game):
+    def get_best_move(self,depth,game):
         best_score=float("-inf")
         best_move=None
         for move in self.get_possible_moves(game):
             game.action(move)
-            score=self.minimax(data,depth,game,False)
+            score=self.minimax(depth,game,False)
             if score>best_score:
                 best_score=score
                 best_move=move
+            game.daction(move)
         return best_move
     def forward(self,*args,**kwargs):
-        data=args[0]
-        deepth=args[1]
-        game=args[2]
-        self.player=args[3]
-        move=self.get_best_move(data,deepth,game)
+        deepth=args[0]
+        game=args[1]
+        self.player=args[2]
+        move=self.get_best_move(deepth,game)
         return move
 if __name__=="__main__":
-   Game=game()
+   game=game()
+   Search = search()
+   game.action([1, 1])
+   import time
+   time1=time.time()
+   Search(game.get_ob(),5,game,game.player)
+   print(time.time()-time1)
    pass
