bd08f4bade12aabcc6d7065cd3096f2e0ed62326
TYJ12138 <142282348+TYJ12138@users.noreply.github.com>
2023-08-18 12:14:46 +0800

    Add files via upload

--- tyj.py
+++ tyj.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from PIL import Image,ImageTk
 
 
 class Window(object):
@@ -9,7 +10,7 @@ class Window(object):
         self.start_game()
 
     def start_game(self):
-        global imag1,imag2
+        global imag1, imag2
         self.lf = tk.LabelFrame(self.master)
         self.lbl = tk.Label(self.lf, image=imag1)
         self.lbl.pack()
@@ -18,6 +19,7 @@ class Window(object):
         self.btn_user = tk.Button(self.lf, text='玩家先手', command=self.create_board)
         self.btn_user.place(x=172, y=267)
         self.lf.pack()
+
     # 开始游戏界面
 
     def create_board(self):
@@ -26,6 +28,10 @@ class Window(object):
         if self.can:
             self.can.destroy()
         self.can = tk.Canvas(self.master, width=300, height=300)
+        self.lbl = tk.Label(self.master, image=imag2)
+        self.lbl.place(x=33,y=35)
+        self.lbl = tk.Label(self.master, image=imag3)
+        self.lbl.place(x=35,y=115)
         self.can.create_line((30, 30), (270, 30), width=2)
         self.can.create_line((270, 30), (270, 270), width=2)
         self.can.create_line((270, 270), (30, 270), width=2)
@@ -40,6 +46,7 @@ class Window(object):
         self.btn1.place(x=105, y=273)
         self.btn2 = tk.Button(self.master, text='保存')
         self.btn2.place(x=120, y=0)
+
     # 绘制棋盘
 
     def circles(self, event):
@@ -48,6 +55,7 @@ class Window(object):
                 if j <= event.x < j + 80 and i <= event.y < i + 80:
                     self.can.create_oval((j, i), (j + 80, i + 80), width=2, outline='red')
                     break
+
     # 点击画圆
 
     def forks(self):
@@ -58,10 +66,16 @@ class Window(object):
                 break
     # 画叉
 
+    def draw(self):
+
+
+
 
 root = tk.Tk()
 root.title('井字棋')
 root.geometry("300x300+550+150")
 imag1 = tk.PhotoImage(file='image/开始界面2.png')
+imag2 = tk.PhotoImage(file='image/圈.png')
+imag3 = tk.PhotoImage(file='image/叉.png')
 Window(root)
 root.mainloop()
