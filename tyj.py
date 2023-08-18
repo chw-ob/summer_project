738400d63bfb7cf74f87f2e067cbf4a05099dd65
TYJ12138 <142282348+TYJ12138@users.noreply.github.com>
2023-08-18 13:56:51 +0800

    Add files via upload

--- tyj.py
+++ tyj.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
-from PIL import Image,ImageTk
+
+import numpy
 
 
 class Window(object):
@@ -8,30 +9,24 @@ class Window(object):
         self.master = master
         self.can = None
         self.start_game()
+        self.board = [('', '', ''), ('', '', ''), ('', '', '')]
 
     def start_game(self):
         global imag1, imag2
         self.lf = tk.LabelFrame(self.master)
-        self.lbl = tk.Label(self.lf, image=imag1)
-        self.lbl.pack()
-        self.btn_computer = tk.Button(self.lf, text='电脑先手', command=self.create_board)
+        self.lbl1 = tk.Label(self.lf, image=imag1)
+        self.lbl1.pack()
+        self.btn_computer = tk.Button(self.lf, text='电脑先手')
         self.btn_computer.place(x=45, y=267)
-        self.btn_user = tk.Button(self.lf, text='玩家先手', command=self.create_board)
+        self.btn_user = tk.Button(self.lf, text='玩家先手')
         self.btn_user.place(x=172, y=267)
         self.lf.pack()
-
     # 开始游戏界面
 
     def create_board(self):
         if self.lf:
             self.lf.destroy()
-        if self.can:
-            self.can.destroy()
         self.can = tk.Canvas(self.master, width=300, height=300)
-        self.lbl = tk.Label(self.master, image=imag2)
-        self.lbl.place(x=33,y=35)
-        self.lbl = tk.Label(self.master, image=imag3)
-        self.lbl.place(x=35,y=115)
         self.can.create_line((30, 30), (270, 30), width=2)
         self.can.create_line((270, 30), (270, 270), width=2)
         self.can.create_line((270, 270), (30, 270), width=2)
@@ -41,34 +36,28 @@ class Window(object):
         for i in range(3):
             self.can.create_line(((i + 1) * 80 + 30, 30), ((i + 1) * 80 + 30, 270), width=2)
         self.can.pack(expand='YES', fill='both')
-        self.can.bind("<ButtonRelease-1>", self.circles)
         self.btn1 = tk.Button(self.master, text='重新开始', command=self.create_board)
         self.btn1.place(x=105, y=273)
         self.btn2 = tk.Button(self.master, text='保存')
         self.btn2.place(x=120, y=0)
-
     # 绘制棋盘
 
-    def circles(self, event):
-        for i in range(30, 191, 80):
-            for j in range(30, 191, 80):
-                if j <= event.x < j + 80 and i <= event.y < i + 80:
-                    self.can.create_oval((j, i), (j + 80, i + 80), width=2, outline='red')
-                    break
+    def creat_chess(self, board):
+        for i in range(3):
+            for j in range(3):
+                if board[i][j]=='0':
+                    self.lbl3 = tk.Label(self.master, image=imag2)
+                    self.lbl3.place(x=33+i*80, y=35+j*80)
+                elif board[i][j]=='1':
+                    self.lbl4 = tk.Label(self.master, image=imag3)
+                    self.lbl4.place(x=33+i*80, y=35+j*80)
+    # 根据数组画棋子
 
-    # 点击画圆
-
-    def forks(self):
-        for i in range(30, 191, 80):
-            for j in range(30, 191, 80):
-                self.can.create_line((j, i), (j + 80, i + 80), width=2, fill='blue')
-                self.can.create_line((j, i + 80), (j + 80, i), width=2, fill='blue')
-                break
-    # 画叉
-
-    def draw(self):
-
-
+    def play(self):
+        #boardcopy=self.board.copy()
+        boardcopy=[('0', '0', '0'), ('1', '1', '0'), ('1', '0', '0')]
+        self.creat_chess(boardcopy)
+    # 测试函数
 
 
 root = tk.Tk()
